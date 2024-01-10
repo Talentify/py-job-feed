c15fcf0ee3547eca8963628946e0f92bc1c21950
Canhota <1024835+antoniocanhota@users.noreply.github.com>
2024-01-10 16:55:32 -0300

    Refactoring

---
+++ src/feed_generator/exporters/xml_exporter.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+
+
+class XMLExporter:
+
+    def export(self, es_response, output_file):
+        # Create XML tree
+        root = ET.Element("results")
+
+        # Add results to XML
+        for hit in es_response['hits']['hits']:
+            result_element = ET.SubElement(root, "result")
+            for key, value in hit['_source'].items():
+                ET.SubElement(result_element, key).text = str(value)
+
+        # Create and save XML file
+        tree = ET.ElementTree(root)
+        tree.write(output_file, encoding="utf-8", xml_declaration=True)
--- src/feed_generator/feed_generator.py
+++ src/feed_generator/feed_generator.py
@@ -1,11 +1,11 @@
 import sys
 
 import click
-import xml.etree.ElementTree as ET
 
 from settings import ELASTICSEARCH_INDEX
 from src.feed_generator.db import job_feed_config_queries
 from src.feed_generator.db.elasticsearch_handler import ElasticSearchHandler
+from src.feed_generator.exporters.xml_exporter import XMLExporter
 
 
 @click.command()
@@ -16,30 +16,12 @@ def main(id):
         print(f"job_feed_config not found with ID {id}")
         sys.exit(1)
 
-    # Output XML file
+    exporter = XMLExporter()
     output_file_name = "output.xml"
-
-    # Execute query and save results to XML
-    execute_query_and_save_to_xml(ELASTICSEARCH_INDEX, job_feed_config.query, output_file_name)
-
-
-def execute_query_and_save_to_xml(index, query, output_file):
-    # Execute the query
     es = ElasticSearchHandler.get_default().client
-    response = es.search(index=index, body=query)
 
-    # Create XML tree
-    root = ET.Element("results")
-
-    # Add results to XML
-    for hit in response['hits']['hits']:
-        result_element = ET.SubElement(root, "result")
-        for key, value in hit['_source'].items():
-            ET.SubElement(result_element, key).text = str(value)
-
-    # Create and save XML file
-    tree = ET.ElementTree(root)
-    tree.write(output_file, encoding="utf-8", xml_declaration=True)
+    response = es.search(index=ELASTICSEARCH_INDEX, body=job_feed_config.query)
+    exporter.export(response, output_file_name)
 
 
 if __name__ == "__main__":
