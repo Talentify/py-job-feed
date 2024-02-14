a8918b34f4d2a9424fca1387f9bc95c0ccf6ebf7
Canhota <1024835+antoniocanhota@users.noreply.github.com>
2024-02-14 17:24:43 -0300

    Pruned fields

--- src/feed_generator/feed_generator/es_hit_formatters/default.py
+++ src/feed_generator/feed_generator/es_hit_formatters/default.py
@@ -5,16 +5,27 @@ class DefaultESHitFormatter(ESHitFormatterABC):
 
     @staticmethod
     def source() -> [str]:
-        return ["id",
+        return ["urls.cover",
                 "description.common",
+                "client.name",
+                "location.is_remote",
+                "location.city_name",
+                "location.region_name",
+                "location.country_iso_code",
+                "location.full_address",
+                "compensation.to_display",
+                "type.to_display",
                 "title",
-                "location.*",
-                "compensation.*",
-                "type.*",
-                "client.*",
-                "urls.*",
                 ]
 
     @staticmethod
     def format(es_hit):
-        return es_hit
+        return {
+            "url": (es_hit.get("urls") or {}).get("cover"),
+            "description": (es_hit.get("description") or {}).get("common"),
+            "company": (es_hit.get("client") or {}).get("name"),
+            "location":  es_hit.get("location"),
+            "compensation": (es_hit.get("compensation") or {}).get("to_display"),
+            "type": (es_hit.get("type") or {}).get("to_display"),
+            "title": es_hit.get("title")
+        }
